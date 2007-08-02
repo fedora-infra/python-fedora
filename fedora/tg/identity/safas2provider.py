b9aac69763e76ba9353f7d53e3c5dc4f273e1daa
Toshio Kuratomi <toshio@tiki-lounge.com>
2007-08-02 11:57:10 -0700

    * fedora/tg/identity/sopwith-fas-identity.py: Remove as we've never used this   for real, just as an example to build ur own identity provider. * Add GPLv2+ statements to all the other source files.

--- fedora/tg/identity/safas2provider.py
+++ fedora/tg/identity/safas2provider.py
@@ -1,15 +1,22 @@
+# python-fedora, python module to interact with Fedora Infrastructure Services
+# Copyright (C) 2007  Red Hat Software Inc
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; version 2 of the License.
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Library General Public License for more details.
+# GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Authors: Mike Mcgrath <mmcgrath@redhat.com>
+#          Toshio Kuratomi <tkuratom@redhat.com>
 
 '''
 This plugin provides authentication of passwords against the Fedora Account
