bbca6404cc972ef8c78e6d30759fcfcbb9e342b5
Toshio くらとみ <toshio@publictest10.fedoraproject.org>
2008-02-11 18:54:58 -0700

    * fedora/tg/visit/, fedora/tg/visit/__init__.py, fedora/tg/visit/safasvisit.py:   Removed the visit plugin as the new TurboGears is supposed to allow for   different databases to be used. * fedora/accounts/tgfas.py: Port to SQLAlchemy 0.4/TG-1.0.4. * fedora/tg/identity/safasprovider.py: New TurboGears is supposed to allow   multiple databases to be used.  Try using that.   - Update methods to be close to the upstream saprovider.  Hopefully the new     upstream code doesn't have the login/logout fails to work bugs that the     original sync did.  If it does we'll have to re-add a bunch of flushes     from our implementation. * setup.py: Remove the safasvisit manager.   - Update our requirement to TurboGears-1.0.4.

--- fedora/tg/identity/safasprovider.py
+++ fedora/tg/identity/safasprovider.py
@@ -24,10 +24,14 @@ System.
 '''
 
 import logging
-from fedora.accounts.fas import AccountSystem
 
-from turbogears.identity.saprovider import *
+from sqlalchemy.orm import class_mapper
+from turbogears.identity.saprovider import SqlAlchemyIdentity, \
+        SqlAlchemyIdentityProvider
 from turbogears import config
+from turbogears.util import load_class
+
+from fedora.accounts.fas import AccountSystem
 from fedora.accounts.tgfas import VisitIdentity
 from fedora.accounts.fas import AuthError
 from fedora.accounts.fas import DBError
@@ -83,12 +87,7 @@ class SaFasIdentity(SqlAlchemyIdentity):
 
         # If the user isn't logged in or we are unable to get information
         # about them from the fas database, return empty None
-        visit_identity_class.mapper.get_session().flush()
-        visit_identity_class.mapper.get_session().clear()
-        try:
-            visit = visit_identity_class.get_by(visit_key = self.visit_key)
-        except:
-            return None
+        visit = visit_identity_class.query.filter_by(visit_key = self.visit_key).first()
         if not visit:
             self._user = None
             return None
@@ -106,27 +105,6 @@ class SaFasIdentity(SqlAlchemyIdentity):
         return self._user
     user = property(_get_user)
 
-    ### FIXME: This is unnecessary once TG Bug #1238 is resolved.
-    # Basically, we do not want to refer to the app-wide session directly as
-    # we could be using a site-wide session.  Use the session associated with
-    # the mapper for the visit class instead.
-    def logout(self):
-        '''
-        Remove the link between this identity and the visit.
-        '''
-        if not self.visit_key:
-            return
-        try:
-            visit = visit_identity_class.get_by(visit_key=self.visit_key)
-            visit.delete()
-        except:
-            pass
-        else:
-            visit_identity_class.mapper.get_session().flush()
-        # Clear the current identity
-        anon = SaFasIdentity(None,None)
-        identity.set_current_identity(anon)
-
 class SaFasIdentityProvider(SqlAlchemyIdentityProvider):
     '''
     IdentityProvider that authenticates users against the fedora account system
@@ -139,7 +117,7 @@ class SaFasIdentityProvider(SqlAlchemyIdentityProvider):
         visit_identity_class = load_class(visit_identity_class_path)
 
     def create_provider_model(self):
-        visit_identity_class.mapper.local_table.create(checkfirst=True)
+        class_mapper(visit_identity_class).local_table.create(checkfirst=True)
 
     def validate_identity(self, user_name, password, visit_key):
         '''
@@ -153,8 +131,6 @@ class SaFasIdentityProvider(SqlAlchemyIdentityProvider):
             groups: a set of group IDs
             permissions: a set of permission IDs
         '''
-        visit_identity_class.mapper.get_session().flush()
-        visit_identity_class.mapper.get_session().clear()
         try:
             verified = fas.verify_user_pass(user_name, password)
         except (AuthError, DBError), e:
@@ -175,13 +151,14 @@ class SaFasIdentityProvider(SqlAlchemyIdentityProvider):
                     % {'err': e, 'user': user_name})
             return None
 
-        link = visit_identity_class.get_by(visit_key=visit_key)
+        link = visit_identity_class.query.filter_by(visit_key=visit_key).first()
         if not link:
-            link = visit_identity_class(visit_key=visit_key, user_id = userId)
-            visit_identity_class.mapper.get_session().save(link)
+            link = visit_identity_class()
+            link.visit_key = visit_key
+            link.user_id = userId
         else:
             link.user_id = userId
-        visit_identity_class.mapper.get_session().flush()
+        session.flush()
         return SaFasIdentity(visit_key)
 
     def validate_password(self, user, user_name, password):
--- setup.py
+++ setup.py
@@ -13,11 +13,10 @@ setup(name='python-fedora',
         packages=find_packages(),
         include_package_data=True,
         #install_requires=['psycopg2'],
-        extras_require = {'tg' : ['TurboGears >= 1.0b']},
+        extras_require = {'tg' : ['TurboGears >= 1.0.4']},
         entry_points = {
             'turbogears.identity.provider' : (
                 'sabugzilla = fedora.tg.identity.sabzprovider:SaBugzillaIdentityProvider [tg]',
                 'safas = fedora.tg.identity.safasprovider:SaFasIdentityProvider [tg]',
-                'safas2 = fedora.tg.identity.safas2provider:SaFas2IdentityProvider [tg]'),
-            'turbogears.visit.manager' : 'safas = fedora.tg.visit.safasvisit:SaFasVisitManager [tg]'}
+                'safas2 = fedora.tg.identity.safas2provider:SaFas2IdentityProvider [tg]')}
         )
