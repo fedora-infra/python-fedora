992519d9dc33ae50232be7c2a2bf235b0d59a90e
Toshio Kuratomi <badger@Clingman>
2008-04-02 19:24:00 -0700

    * fedora/tg/client.py: Add a DeprecationWarning.

--- fedora/tg/client.py
+++ fedora/tg/client.py
@@ -2,5 +2,8 @@
 
 The canonical location for this module from 0.3 on is fedora.client
 '''
+import warnings
+warnings.warn('fedora.tg.client has moved to fedora.client.'
+        '  This will disappear in 0.4', DeprecationWarning, stacklevel=2)
 
 from fedora.client import *
