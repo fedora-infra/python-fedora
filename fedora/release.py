0f1466f393175ce938628694e61b953866217397
Toshio Kuratomi <badger@Clingman>
2008-05-14 18:17:01 -0700

    * NEWS, fedora/release.py, python-fedora.spec: Update for the new release.

--- fedora/release.py
+++ fedora/release.py
@@ -2,7 +2,7 @@
 Information about this python-fedora release
 '''
 NAME = 'python-fedora'
-VERSION = '0.2.99.10'
+VERSION = '0.2.99.11'
 DESCRIPTION = 'Python modules for interacting with Fedora services'
 LONG_DESCRIPTION = '''
 The Fedora Project runs many different services.  These services help us to
