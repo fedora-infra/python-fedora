fb7de5452ebf09df54303669bc0b0b0a8df4643a
Toshio Kuratomi <badger@Clingman>
2008-06-15 01:19:00 -0400

    * Move BaseClient and ProxyClient to their own files.  Import them into __init__ and use __all__ to make them appear properly.

---
+++ fedora/client/baseclient.py
@@ -0,0 +1,204 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2007  Red Hat, Inc. All rights reserved.
+#
+# This copyrighted material is made available to anyone wishing to use, modify,
+# copy, or redistribute it subject to the terms and conditions of the GNU
+# General Public License v.2.  This program is distributed in the hope that it
+# will be useful, but WITHOUT ANY WARRANTY expressed or implied, including the
+# implied warranties of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.  You should have
+# received a copy of the GNU General Public License along with this program;
+# if not, write to the Free Software Foundation, Inc., 51 Franklin Street,
+# Fifth Floor, Boston, MA 02110-1301, USA. Any Red Hat trademarks that are
+# incorporated in the source code or documentation are not subject to the GNU
+# General Public License and may only be used or replicated with the express
+# permission of Red Hat, Inc.
+#
+# Red Hat Author(s): Luke Macken <lmacken@redhat.com>
+#                    Toshio Kuratomi <tkuratom@redhat.com>
+#
+import os
+import stat
+from os import path
+import logging
+import cPickle as pickle
+
+from fedora import __version__
+from fedora import _
+
+log = logging.getLogger(__name__)
+
+SESSION_FILE = path.join(path.expanduser('~'), '.fedora_session')
+
+from fedora.client import AuthError
+from fedora.client import ProxyClient
+
+class BaseClient(ProxyClient):
+    '''
+        A command-line client to interact with Fedora TurboGears Apps.
+    '''
+    def __init__(self, base_url, useragent=None, debug=False, username=None,
+            password=None, session_cookie=None, cache_session=True):
+        '''
+        Arguments:
+        :base_url: Base of every URL used to contact the server
+
+        Keyword Arguments:
+        :useragent: useragent string to use.  If not given, default to
+            "Fedora BaseClient/VERSION"
+        :debug: If True, log debug information
+        :username: username for establishing authenticated connections
+        :password: password to use with authenticated connections
+        :session_cookie: user's session_cookie to connect to the server
+        :cache_session: if set to true, cache the user's session cookie on the
+            filesystem between runs.
+        '''
+        self.useragent = useragent or 'Fedora BaseClient/%(version)s' % {
+                'version': __version__}
+        super(BaseClient, self).__init__(base_url, useragent=useragent,
+                debug=debug)
+        self.username = username
+        self.password = password
+        if session_cookie:
+            self.session_cookie = session_cookie
+        else:
+            self._load_session()
+
+        if username and password and not self.session_cookie:
+            self._authenticate(force=True)
+
+    def __load_cookies(self):
+        '''load cookie data from a file.
+
+        Returns: the complete mapping of users to session cookies.
+        '''
+        saved_session = {}
+        if path.isfile(SESSION_FILE):
+            session_file = file(SESSION_FILE, 'r')
+            try:
+                saved_session = pickle.load(session_file)
+            except EOFError:
+                log.info(_('Unable to load session from %(file)s') % \
+                        {'file': SESSION_FILE})
+            session_file.close()
+
+        return saved_session
+
+    def __save_cookies(self, save):
+        '''Save the cached cookies file.
+
+        Arguments:
+        :save: The dict of usernames to cookies to save.
+        '''
+        try:
+            session_file = file(SESSION_FILE, 'w')
+            os.chmod(SESSION_FILE, stat.S_IRUSR | stat.S_IWUSR)
+            pickle.dump(save, session_file)
+            session_file.close()
+        except Exception, e: # pylint: disable-msg=W0703
+            # If we can't save the file, issue a warning but go on.  The
+            # session just keeps you from having to type your password over
+            # and over.
+            log.warning(_('Unable to write to session file %(session)s:' \
+                    ' %(error)s') % {'session': SESSION_FILE, 'error': str(e)})
+
+    def _get_session_cookie(self):
+        '''Attempt to retrieve the session cookie from the filesystem.'''
+        # If none exists, return None
+        if self._session_cookie:
+            return self._session_cookie
+        saved_sessions = self.__load_cookies()
+        self._session_cookie = saved_sessions.get(self.username, '')
+        if not self._session_cookie:
+            log.debug(_('No session cached for "%s"') % self.username)
+
+        return self._session_cookie
+
+    def _set_session_cookie(self, session_cookie):
+        '''Store our pickled session cookie.
+
+        Arguments:
+        :session_cookie: cookie to set our internal cookie to
+
+        This method loads our existing session file and modifies our
+        current user's cookie.  This allows us to retain cookies for
+        multiple users.
+        '''
+        # Start with the previous users
+        if self.username:
+            save = self.__load_cookies()
+            save[self.username] = session_cookie
+            # Save the cookies to the filesystem
+            self.__save_cookies(save)
+        self._session_cookie = session_cookie
+
+    def _del_session_cookie(self):
+        '''Delete the session cookie from the filesystem.'''
+        # Start with the previous users
+        save = self.__load_cookies()
+        try:
+            del save[self.username]
+        except KeyError:
+            # This is fine we just want the session to exist in the new
+            # session file.
+            pass
+        else:
+            # Save the cookies to the filesystem
+            self.__save_cookies(save)
+        self._session_cookie = None
+
+    session_cookie = property(_get_session_cookie, _set_session_cookie, 
+            _del_session_cookie, '''The session_cookie.
+
+        The session cookie is saved in a file in case it is needed in
+        consecutive runs of BaseClient.
+        ''')
+
+    def logout(self):
+        '''Logout from the server.
+        '''
+        try:
+            self.send_request('logout', auth=True)
+        except AuthError: # pylint: disable-msg=W0704
+            # We don't need to fail for an auth error as we're getting rid of
+            # our authentication tokens here.
+            pass
+        del(self.session_cookie)
+
+    def send_request(self, method, req_params=None, auth=True):
+        '''Make an HTTP request to a server method.
+
+        The given method is called with any parameters set in req_params.  If
+        auth is True, then the request is made with an authenticated session
+        cookie.
+
+        Arguments:
+        :method: Method to call on the server.  It's a url fragment that comes
+            after the base_url set in __init__().
+        :req_params: Extra parameters to send to the server.
+        :auth: If True perform auth to the server, else do not.
+        '''
+        if auth == True:
+            # We need something to do auth.  Check user/pass
+            if not self.username and self.password:
+                # No?  Check for session_cookie
+                if not self.session_cookie:
+                    raise AuthError, 'Auth was requested but no way to' \
+                            ' perform auth was given.  Please set username' \
+                            ' and password or session_cookie before calling' \
+                            ' this function with auth=True'
+
+        auth_params = {'username': self.username, 'password': self.password,
+                'cookie': self.session_cookie}
+        # Remove empty params
+        map(auth_params.__delitem__, (key for key, value in
+                auth_params.items() if not value))
+
+        session_cookie, data = super(BaseClient, self).send_request(method,
+                req_params=req_params, auth_params=auth_params)
+        # In case the server returned a new session cookie to us
+        if self.session_cookie != session_cookie:
+            self.session_cookie = session_cookie
+
+        return data
---
+++ fedora/client/proxyclient.py
@@ -0,0 +1,195 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2007  Red Hat, Inc. All rights reserved.
+#
+# This copyrighted material is made available to anyone wishing to use, modify,
+# copy, or redistribute it subject to the terms and conditions of the GNU
+# General Public License v.2.  This program is distributed in the hope that it
+# will be useful, but WITHOUT ANY WARRANTY expressed or implied, including the
+# implied warranties of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.  You should have
+# received a copy of the GNU General Public License along with this program;
+# if not, write to the Free Software Foundation, Inc., 51 Franklin Street,
+# Fifth Floor, Boston, MA 02110-1301, USA. Any Red Hat trademarks that are
+# incorporated in the source code or documentation are not subject to the GNU
+# General Public License and may only be used or replicated with the express
+# permission of Red Hat, Inc.
+#
+# Red Hat Author(s): Luke Macken <lmacken@redhat.com>
+#                    Toshio Kuratomi <tkuratom@redhat.com>
+#
+import Cookie
+import urllib
+import urllib2
+import logging
+import simplejson
+from urlparse import urljoin
+from fedora import __version__
+from fedora import _
+
+log = logging.getLogger(__name__)
+
+class ProxyClient(object):
+    '''
+    A client to a Fedora Service.  This class is optimized for use as a proxy
+    multiple users to a service.  The ProxyClient is designed to be threadsafe
+    so that code can instantiate one instance of the class and use it for
+    multiple requests for different users from different threads.
+    '''
+    def __init__(self, base_url, useragent=None, debug=False):
+        '''Create a client configured for a particular service.
+
+        Arguments:
+        :base_url: Base of every URL used to contact the server
+
+        Keyword Arguments:
+        :useragent: useragent string to use.  If not given, default to
+            "Fedora ProxyClient/VERSION"
+        :debug: If True, log debug information
+        '''
+        if base_url[-1] != '/':
+            base_url = base_url +'/'
+        self.base_url = base_url
+        self.useragent = useragent or 'Fedora ProxyClient/%(version)s' % {
+                'version': __version__}
+
+        # Setup our logger
+        self._log_handler = logging.StreamHandler()
+        self.debug = debug
+        format = logging.Formatter("%(message)s")
+        self._log_handler.setFormatter(format)
+        log.addHandler(self._log_handler)
+
+    def __get_debug(self):
+        '''Return whether we have debug logging turned on.
+        '''
+        if self._log_handler.level <= logging.DEBUG:
+            return True
+        return False
+
+    def __set_debug(self, debug=False):
+        '''Set to return debugging information.
+        '''
+        if debug:
+            log.setlevel(logging.DEBUG)
+            self._log_handler.setLevel(logging.DEBUG)
+        else:
+            log.setLevel(logging.ERROR)
+            self._log_handler.setLevel(logging.INFO)
+
+    debug = property(__get_debug, __set_debug, doc='''
+    When True, we log extra debugging statements.  When False, we only log
+    errors.
+    ''')
+
+    def send_request(self, method, req_params=None, auth_params=None):
+        '''Make an HTTP request to a server method.
+
+        The given method is called with any parameters set in req_params.  If
+        auth is True, then the request is made with an authenticated session
+        cookie.  Note that path parameters should be set by adding onto the
+        method, not via ``req_params``.
+
+        Arguments:
+        :method: Method to call on the server.  It's a url fragment that comes
+            after the base_url set in __init__().  Note that any parameters set
+            as extra path information should be listed here, not in req_params.
+
+        Keyword Arguments:
+        :req_params: dict containing extra parameters to send to the server.
+        :auth_params: dict containing one or more means of authenticating to
+            the server.  Valid entries in this dict are:
+            :cookie: A Cookie.SimpleCookie to send as a session cookie to the
+                server.
+            :username: Username to send to the server.
+            :password: Password to use with username to send to the server.
+            Note that cookie can be sent alone but if one of username or
+            password is set the other must as well.  Code can set all of these
+            if it wants and all of them will be sent to the server.  Be careful
+            of sending cookies that do not match with the username in this
+            case as the server can decide what to do in this case.
+
+        Returns: a tuple of data and session_cookie returned from the server.
+        '''
+        # Check whether we need to authenticate for this request
+        if auth_params:
+            cookie = None
+            username = None
+            if cookie in auth_params:
+                session_cookie = auth_params['cookie']
+            if username in auth_params and password in auth_params:
+                username = auth_params['username']
+                password = auth_params['password']
+            elif username in auth_params or password in auth_params:
+                raise AuthError, _('username and password must both be set in'
+                        ' auth_params')
+            if not cookie or username:
+                raise AuthError, _('No known authentication methods specified:'
+                        ' set "cookie" in auth_params or set both username and'
+                        ' password in auth_params')
+
+        # urljoin is slightly different than os.path.join().  Make sure method
+        # will work with it.
+        method = method.lstrip('/')
+        # And join to make our url.
+        # Note: ?tg_format=json is going away in the future as the Accept
+        # header should serve the same purpose in a more framework neutral
+        # manner.
+        url = urljoin(self.base_url, method + '?tg_format=json')
+
+        response = None # the JSON that we get back from the server
+        data = None     # decoded JSON via simplejson.load()
+
+        log.debug(_('Creating request %(url)s') % {'url': url})
+        req = urllib2.Request(url)
+        req.add_header('User-agent', self.useragent)
+        req.add_header('Accept', 'text/javascript')
+
+        if req_params:
+            req.add_data(urllib.urlencode(req_params))
+        if username and password:
+            # Adding this to the request data prevents it from being logged by
+            # apache.
+            req.add_data(urllib.urlencode({'user_name': username,
+                    'password': password, 'login': 'Login'}))
+        if session_cookie:
+            # Anytime the cookie exists, send it so that visit tracking works.
+            # Will also authenticate us if there's a need.
+            req.add_header('Cookie', session_cookie.output(attrs=[],
+                header='').strip())
+
+        try:
+            response = urllib2.urlopen(req)
+        except urllib2.HTTPError, e:
+            if e.msg == 'Forbidden':
+                # Wrong username or password
+                log.error(e)
+                raise AuthError, _('Unable to log into server.  Invalid'
+                        ' authentication tokens.  Send new username and'
+                        ' password:  %(error)s') % {'error': str(e)}
+            else:
+                raise
+
+        # In case the server returned a new session cookie to us
+        new_session_cookie = Cookie.SimpleCookie()
+        try:
+            new_session_cookie.load(response.headers['set-cookie'])
+        except (KeyError, AttributeError): # pylint: disable-msg=W0704
+            # It's okay if the server didn't send a new cookie
+            pass
+
+        # Read the response
+        json_string = response.read()
+        try:
+            data = simplejson.loads(json_string)
+        except ValueError, e:
+            # The response wasn't JSON data
+            raise ServerError, 'Error returned from simplejson while' \
+                    ' processing %s: %s' % (url, str(e))
+
+        if 'exc' in data:
+            raise AppError(name = data['exc'], message = data['tg_flash'])
+
+        return new_session_cookie, data
+
+
