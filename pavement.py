c805d6b9bf4ae1904790539f2aeda7f049d877d8
Toshio Kuratomi <badger@Clingman>
2008-08-09 10:32:22 -0700

    Move from using setup.py/setuptools to manage builds to paver.

--- pavement.py
+++ pavement.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python -tt
 
+from paver.defaults import *
 from fedora.release import *
 
+import paver.doctools
 from setuptools import setup, find_packages
 
-setup(name=NAME,
+options(
+    setup = Bunch(
+        name=NAME,
         version=VERSION,
         description=DESCRIPTION,
         author=AUTHOR,
@@ -15,12 +19,25 @@ setup(name=NAME,
         download_url=DOWNLOAD_URL,
         packages=find_packages(),
         include_package_data=True,
-        #install_requires=['psycopg2'],
-        extras_require = {'tg' : ['TurboGears >= 1.0.4']},
+        # non-setuptools package.  When everything we care about uses
+        # python-2.5 distutils we can add these:
+        #   for bodhi (koji yum)
+        install_requires=['simplejson'],
+        # And these to extra_require:
+        #   for widgets: (bugzilla feedparser)
+        extras_require = {'tg' : ['TurboGears >= 1.0.4', 'SQLAlchemy']},
         entry_points = {
             'turbogears.identity.provider' : (
                 'jsonfas = fedora.tg.identity.jsonfasprovider:JsonFasIdentityProvider [tg]'),
             'turbogears.visit.manager' : (
                 'jsonfas = fedora.tg.visit.jsonfasvisit:JsonFasVisitManager [tg]')
             }
+        ),
+    sphinx=Bunch(
+        docroot='.',
+        builddir='build-doc',
+        sourcedir='doc'
         )
+
+
+    )
