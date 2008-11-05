469729c9e9c4f9128a99f9c56aab3f47a167464e
Toshio Kuratomi <toshio@fedoraproject.org>
2008-11-05 11:15:44 -0800

    Update with a bugzilla email for romal.

--- fedora/client/fas2.py
+++ fedora/client/fas2.py
@@ -130,6 +130,8 @@ class AccountSystem(BaseClient):
                 102952: 'jmccann@redhat.com',
                 # Simon Wesp: simon@w3sp.de
                 109464: 'cassmodiah@fedoraproject.org',
+                # Robert M. Albrecht: romal@gmx.de
+                101475: 'mail@romal.de',
                 }
         # A few people have an email account that is used in owners.list but
         # have setup a bugzilla account for their primary account system email
