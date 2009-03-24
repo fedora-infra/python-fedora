36bc222b29c0e16e7aa729ee9dc3cd3556a5fd90
Toshio Kuratomi <toshio@fedoraproject.org>
2009-03-23 20:12:29 -0700

    * Build and install message catalogs. * Port to paver-1.0

--- pavement.py
+++ pavement.py
@@ -1,14 +1,23 @@
-from paver.defaults import *
+try:
+    # 1.0
+    from paver.easy import path as paver_path
+    from paver.easy import sh as paver_sh
+    from paver.easy import *
+    import paver.misctasks
+    from paver import setuputils
+    setuputils.install_distutils_tasks()
+except ImportError:
+    # 0.8
+    from paver.defaults import *
+    from paver.runtime import path as paver_path
+    from paver.runtime import sh as paver_sh
 
 import sys, os
 import glob
 import paver.doctools
-import paver.runtime
-import paver.path
+from setuptools import find_packages, command
 
-from setuptools import find_packages
-
-sys.path.insert(0, str(paver.runtime.path.getcwd()))
+sys.path.insert(0, str(paver_path.getcwd()))
 
 from fedora.release import *
 
@@ -41,6 +50,10 @@ options(
                 'jsonfas = fedora.tg.visit.jsonfasvisit1:JsonFasVisitManager [tg]',
                 'jsonfas2 = fedora.tg.visit.jsonfasvisit2:JsonFasVisitManager [tg]'),
             },
+        message_extractors = {
+            'fedora': [('**.py', 'python', None),
+                ('tg/templates/genshi/**.html', 'genshi', None),],
+            },
         classifiers = [
             'Development Status :: 4 - Beta',
             'Framework :: TurboGears',
@@ -65,6 +78,11 @@ options(
         doc_location='fedorahosted.org:/srv/web/releases/p/y/python-fedora/doc/',
         tarball_location='fedorahosted.org:/srv/web/releases/p/y/python-fedora/'
         ),
+    i18n=Bunch(
+        builddir='locale',
+        installdir='/usr/share/locale',
+        domain='python-fedora',
+        ),
     )
 
 @task
@@ -72,7 +90,7 @@ options(
 def publish_doc():
     options.order('publish', add_rest=True)
     command = 'rsync -av build-doc/html/ %s' % (options.doc_location,)
-    dry(command, paver.runtime.sh, [command])
+    dry(command, paver_sh, [command])
 
 @task
 @needs(['sdist'])
@@ -80,12 +98,113 @@ def publish_tarball():
     options.order('publish', add_rest=True)
     tarname = '%s-%s.tar.gz' % (options.name, options.version)
     command = 'scp dist/%s %s' % (tarname, options.tarball_location)
-    dry(command, paver.runtime.sh, [command])
+    dry(command, paver_sh, [command])
 
 @task
 @needs(['publish_doc', 'publish_tarball'])
 def publish():
     pass
+
+try:
+    import babel.messages.frontend
+    has_babel = True
+except ImportError:
+    has_babel = False
+
+if has_babel:
+    @task
+    def make_catalogs():
+        '''Compile all message catalogs for release'''
+        options.order('i18n', add_rest=True)
+        for po_file in glob.glob('po/*.po'):
+            locale, ext = os.path.splitext(os.path.basename(po_file))
+            build_dir = paver_path(os.path.join(options.builddir, locale,
+                'LC_MESSAGES'))
+
+            try:
+                build_dir.makedirs()
+            except OSError, e:
+                # paver < 1.0 raises if directory exists
+                if e.errno == 17:
+                    pass
+                else:
+                    raise
+            if 'compile_catalog' in paver.tasks.environment.options:
+                defaults = paver.tasks.environment.options['compile_catalog']
+            else:
+                defaults = Bunch(domain=options.domain,
+                        directory=options.builddir)
+                paver.tasks.environment.options['compile_catalog'] = defaults
+
+            defaults.update({'input-file': po_file, 'locale': locale})
+            ### FIXME: compile_catalog cannot handle --dry-run on its own
+            dry('paver compile_catalog -D %(domain)s -d %(directory)s'
+                    ' -i %(input-file)s --locale %(locale)s' % defaults,
+                    call_task, 'babel.messages.frontend.compile_catalog')
+
+def _install_catalogs(args):
+    '''Install message catalogs in their proper location on the filesystem.
+
+    Note: To use this with non-default commandline arguments, you must use 
+    '''
+    # Rebuild message catalogs
+    if 'skip-build' not in args:
+        call_task('make_catalogs')
+
+    options.order('i18n', add_rest=True)
+    # Setup the install_dir
+    if 'install_catalogs' in args:
+        cat_dir = args['install_catalogs']
+    elif 'install_data' in args:
+        cat_dir = os.path.join(args['install_data'], 'locale')
+    else:
+        cat_dir = options.installdir
+
+    if 'root' in args:
+        if cat_dir.startswith('/'):
+            cat_dir = cat_dir[1:]
+        cat_dir = paver_path(os.path.join(args['root'], cat_dir))
+
+    for catalog in paver_path(options.builddir).walkfiles('*.mo'):
+        locale_dir = catalog.dirname()
+        path = paver_path('.')
+        for index, nextpath in enumerate(locale_dir.splitall()):
+            path = path.joinpath(nextpath)
+            if paver_path(options.builddir).samefile(path):
+                install_locale = cat_dir.joinpath(os.path.join(
+                        *locale_dir.splitall()[index + 1:]))
+                try:
+                    install_locale.makedirs()
+                except OSError, e:
+                    # paver < 1.0 raises if directory exists
+                    if e.errno == 17:
+                        pass
+                    else:
+                        raise
+                install_locale = install_locale.joinpath(catalog.basename())
+                if install_locale.exists():
+                    install_locale.remove()
+                catalog.copy(install_locale)
+
+@task
+@cmdopts([('root=', None, 'Base root directory to install into'),
+    ('install-catalogs=', None, 'directory that locale catalogs go in'),
+    ('skip-build', None, 'Skip directly to installing'),
+    ])
+def install_catalogs():
+    _install_catalogs(options.install_catalogs)
+    pass
+
+@task
+@needs(['make_catalogs', 'setuptools.command.sdist'])
+def sdist():
+    pass
+
+@task
+@needs(['setuptools.command.install'])
+def install():
+    '''Override the setuptools install.'''
+    _install_catalogs(options.install)
 
 #
 # Generic Tasks
