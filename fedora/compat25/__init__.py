0f5557843c303017d52f3203d2c2874618ddbca8
Toshio Kuratomi <toshio@fedoraproject.org>
2010-09-05 16:40:05 -0400

    Deprecate fedora.compat25.defaultdict in favor of kitchen.pycompat25.collections.defaultdict

--- fedora/compat25/__init__.py
+++ fedora/compat25/__init__.py
@@ -1,3 +1,15 @@
 '''A module to implement python2.5 functionality for python programs less
 than 2.5.
 '''
+# Deprecate in favor of kitchen.pycompat25.collections.defaultdict
+import warnings
+
+from fedora import b_
+
+warnings.warn(b_('fedora.pycompat25.defaultdict is deprecated, use'
+    ' kitchen.pycompat25.collections.defaultdict instead'),
+    DeprecationWarning, stacklevel=2)
+
+from kitchen.pycompat25.collections import defaultdict
+
+__all__ = ('defaultdict',)
