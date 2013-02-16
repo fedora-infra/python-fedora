71fe847ed57dfc136e53d8a2fb12d08b39ee914e
Patrick Uiterwijk <puiterwijk@gmail.com>
2013-02-16 21:50:54 +0100

    Add a note what the fedora._openid_extensions module is for

--- fedora/_openid_extensions/__init__.py
+++ fedora/_openid_extensions/__init__.py
@@ -1,1 +1,2 @@
-# This exists just to tshow it is a python module
+# This exists just to show it is a python module
+# These modules are only here temporarily while we wait for them to appear in upstream python-openid
