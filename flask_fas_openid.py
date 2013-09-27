a8ddfb61015a880501dc1e5d2f345a2823fa8072
Patrick Uiterwijk <puiterwijk@redhat.com>
2013-09-27 08:48:38 +0200

    Inject at the right place

--- flask_fas_openid.py
+++ flask_fas_openid.py
@@ -44,12 +44,26 @@ from openid_teams import teams
 
 from fedora import __version__
 
-class FAS(object):
 
-    def __init__(self, app=None):
-        self.app = app
-        if self.app is not None:
-            self._init_app(app)
+class ReverseProxied(object):
+    '''Wrap the application in this middleware and configure the
+    front-end server to add these headers, to let you quietly bind
+    this to a URL other than / and to an HTTP scheme that is
+    different than what is used locally.
+
+    In nginx:
+    location /myprefix {
+        proxy_pass http://192.168.0.1:5001;
+        proxy_set_header Host $host;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Scheme $scheme;
+        proxy_set_header X-Script-Name /myprefix;
+        }
+
+    :param app: the WSGI application
+    '''
+    def __init__(self, app):
+        self.app = app 
 
     def __call__(self, environ, start_response):
         script_name = environ.get('HTTP_X_SCRIPT_NAME', '') 
@@ -64,6 +78,13 @@ class FAS(object):
             environ['wsgi.url_scheme'] = scheme
         return self.app(environ, start_response)
 
+
+class FAS(object):
+    def __init__(self, app=None):
+        app.wsgi_app = ReverseProxied(app.wsgi_app)
+        self.app = app
+        if self.app is not None:
+            self._init_app(app)
 
     def _init_app(self, app):
         app.config.setdefault('FAS_OPENID_ENDPOINT',
