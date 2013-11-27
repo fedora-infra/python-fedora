2729a0ac74cd612a014950c1dd8dae3b8f3a0c04
Toshio Kuratomi <toshio@fedoraproject.org>
2013-11-27 13:44:03 -0800

    Update minimum version from python-2.5 to 2.6.  Since we're targetting RHEL6 and higher, this makes sense.

--- setup.py
+++ setup.py
@@ -59,7 +59,6 @@ setup(
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
-        'Programming Language :: Python :: 2.5',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Topic :: Internet :: WWW/HTTP',
