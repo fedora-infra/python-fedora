494d326e554389f12fee978cf0e224dd0cdee34a
John Anderson <sontek@gmail.com>
2013-12-22 15:55:42 -0800

    PEP8 tg1utils.py

--- fedora/tg/tg1utils.py
+++ fedora/tg/tg1utils.py
@@ -2,17 +2,17 @@
 #
 # Copyright (C) 2011  Red Hat, Inc.
 # This file is part of python-fedora
-# 
+#
 # python-fedora is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
 # version 2.1 of the License, or (at your option) any later version.
-# 
+#
 # python-fedora is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Lesser General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Lesser General Public
 # License along with python-fedora; if not, see <http://www.gnu.org/licenses/>
 #
@@ -30,14 +30,15 @@ Miscellaneous functions of use on a TurboGears Server.  This interface is
 #   tg/utils.  We use a wildcard import just to import the names for
 #   functions into this namespace.
 # :W0614: Ditto.
-from fedora.tg.utils import * #pylint:disable-msg=W0614,W0401
+from fedora.tg.utils import *  # pylint:disable-msg=W0614,W0401
 
 import warnings
 
-warnings.warn('fedora.tg.tg1utils is deprecated.  Switch to'
+warnings.warn(
+    'fedora.tg.tg1utils is deprecated.  Switch to'
     ' fedora.tg.utils instead.  This file will disappear in 0.4',
     DeprecationWarning, stacklevel=2)
 
 __all__ = ('add_custom_stdvars', 'absolute_url', 'enable_csrf',
-                'fedora_template', 'jsonify_validation_errors', 'json_or_redirect',
-                        'request_format', 'tg_absolute_url', 'tg_url', 'url')
+           'fedora_template', 'jsonify_validation_errors', 'json_or_redirect',
+           'request_format', 'tg_absolute_url', 'tg_url', 'url')
