151f75d459f52a500ea02f6fce3d96b6ee427c51
John Anderson <sontek@gmail.com>
2013-12-22 16:30:54 -0800

    PEP8 backends.py

--- fedora/django/auth/backends.py
+++ fedora/django/auth/backends.py
@@ -2,12 +2,12 @@
 #
 # Copyright (C) 2009  Ignacio Vazquez-Abrams
 # This file is part of python-fedora
-# 
+#
 # python-fedora is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
 # version 2.1 of the License, or (at your option) any later version.
-# 
+#
 # python-fedora is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
@@ -28,9 +28,10 @@ from fedora.django.auth.models import FasUser
 from django.contrib.auth.models import AnonymousUser
 from django.contrib.auth.backends import ModelBackend
 
+
 class FasBackend(ModelBackend):
     def authenticate(self, username=None, password=None,
-        session_id=None):
+                     session_id=None):
         try:
             if session_id:
                 auth = {'session_id': session_id}
