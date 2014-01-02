9b7b949b49815903377287299ae6fcef8c171562
John Anderson <sontek@gmail.com>
2014-01-01 18:29:56 -0800

    Re-indent to keep start on same line

--- fedora/accounts/fas2.py
+++ fedora/accounts/fas2.py
@@ -4,9 +4,9 @@ The canonical location for this module from 0.3 on is fedora.client.fas2
 '''
 import warnings
 
-warnings.warn(
-    'fedora.accounts.fas2 has moved to fedora.client.fas2.  This'
-    ' location will disappear in 0.4', DeprecationWarning, stacklevel=2)
+warnings.warn('fedora.accounts.fas2 has moved to fedora.client.fas2.  This'
+              ' location will disappear in 0.4', DeprecationWarning,
+              stacklevel=2)
 
 # pylint: disable-msg=W0401,W0614
 from fedora.client.fas2 import *
