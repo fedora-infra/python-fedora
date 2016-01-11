fedora-infra/python-fedora

commit 49fd63759836725d15affad68621321b012bba0f

Do not interpolate bodhi template values

fedora/client/bodhi.py
@@ -356,17 +356,20 @@ class Bodhi2Client(OpenIdBaseClient):
 
         for section in config.sections():
             update = {
-                'builds': section, 'bugs': config.get(section, 'bugs'),
-                'close_bugs': config.getboolean(section, 'close_bugs'),
-                'type': config.get(section, 'type'),
-                'type_': config.get(section, 'type'),
-                'request': config.get(section, 'request'),
-                'severity': config.get(section, 'severity'),
-                'notes': config.get(section, 'notes'),
-                'autokarma': config.get(section, 'autokarma'),
-                'stable_karma': config.get(section, 'stable_karma'),
-                'unstable_karma': config.get(section, 'unstable_karma'),
-                'suggest': config.get(section, 'suggest'),
+                'builds': section,
+                'bugs': config.get(section, 'bugs', raw=True),
+                'close_bugs': config.getboolean(
+                    section, 'close_bugs', raw=True),
+                'type': config.get(section, 'type', raw=True),
+                'type_': config.get(section, 'type', raw=True),
+                'request': config.get(section, 'request', raw=True),
+                'severity': config.get(section, 'severity', raw=True),
+                'notes': config.get(section, 'notes', raw=True),
+                'autokarma': config.get(section, 'autokarma', raw=True),
+                'stable_karma': config.get(section, 'stable_karma', raw=True),
+                'unstable_karma': config.get(
+                    section, 'unstable_karma', raw=True),
+                'suggest': config.get(section, 'suggest', raw=True),
                 }
 
             updates.append(update)
@@ -791,17 +794,20 @@ class Bodhi1Client(BaseClient):
             for section in config.sections():
                 update = {}
                 update['builds'] = section
-                update['type_'] = config.get(section, 'type')
-                update['request'] = config.get(section, 'request')
-                update['bugs'] = config.get(section, 'bugs')
-                update['close_bugs'] = config.getboolean(section, 'close_bugs')
-                update['notes'] = config.get(section, 'notes')
-                update['autokarma'] = config.getboolean(section, 'autokarma')
-                update['stable_karma'] = config.getint(section, 'stable_karma')
-                update['unstable_karma'] = config.getint(section,
-                                                         'unstable_karma')
-                update['suggest_reboot'] = config.getboolean(section,
-                                                             'suggest_reboot')
+                update['type_'] = config.get(section, 'type', raw=True)
+                update['request'] = config.get(section, 'request', raw=True)
+                update['bugs'] = config.get(section, 'bugs', raw=True)
+                update['close_bugs'] = config.getboolean(
+                    section, 'close_bugs', raw=True)
+                update['notes'] = config.get(section, 'notes', raw=True)
+                update['autokarma'] = config.getboolean(
+                    section, 'autokarma', raw=True)
+                update['stable_karma'] = config.getint(
+                    section, 'stable_karma', raw=True)
+                update['unstable_karma'] = config.getint(
+                    section, 'unstable_karma', raw=True)
+                update['suggest_reboot'] = config.getboolean(
+                    section, 'suggest_reboot', raw=True)
                 updates.append(update)
         return updates
 

commit 6808827c92e1bcfa74de90374a3a970eff892049

configparser.getboolean,getint do not have the raw kwarg
Regression from 49fd63759836725d15affad68621321b012bba0f

fedora/client/bodhi.py
@@ -347,8 +347,7 @@ class Bodhi2Client(OpenIdBaseClient):
             update = {
                 'builds': section,
                 'bugs': config.get(section, 'bugs', raw=True),
-                'close_bugs': config.getboolean(
-                    section, 'close_bugs', raw=True),
+                'close_bugs': config.getboolean(section, 'close_bugs'),
                 'type': config.get(section, 'type', raw=True),
                 'type_': config.get(section, 'type', raw=True),
                 'request': config.get(section, 'request', raw=True),
@@ -786,17 +785,14 @@ class Bodhi1Client(BaseClient):
                 update['type_'] = config.get(section, 'type', raw=True)
                 update['request'] = config.get(section, 'request', raw=True)
                 update['bugs'] = config.get(section, 'bugs', raw=True)
-                update['close_bugs'] = config.getboolean(
-                    section, 'close_bugs', raw=True)
+                update['close_bugs'] = config.getboolean(section, 'close_bugs')
                 update['notes'] = config.get(section, 'notes', raw=True)
-                update['autokarma'] = config.getboolean(
-                    section, 'autokarma', raw=True)
-                update['stable_karma'] = config.getint(
-                    section, 'stable_karma', raw=True)
+                update['autokarma'] = config.getboolean(section, 'autokarma')
+                update['stable_karma'] = config.getint(section, 'stable_karma')
                 update['unstable_karma'] = config.getint(
-                    section, 'unstable_karma', raw=True)
+                    section, 'unstable_karma')
                 update['suggest_reboot'] = config.getboolean(
-                    section, 'suggest_reboot', raw=True)
+                    section, 'suggest_reboot')
                 updates.append(update)
         return updates
 