4dae731b00bd937ce01844a5a32462a54fcb624b
Patrick Uiterwijk <patrick@puiterwijk.org>
2018-02-01 12:24:32 +0100

    Bump version to 0.10.0
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- fedora/release.py
+++ fedora/release.py
@@ -4,7 +4,7 @@ Information about this python-fedora release
 
 
 NAME = 'python-fedora'
-VERSION = '0.9.0'
+VERSION = '0.10.0'
 DESCRIPTION = 'Python modules for interacting with Fedora Services'
 LONG_DESCRIPTION = '''
 The Fedora Project runs many different services.  These services help us to
@@ -13,7 +13,7 @@ This package contains software that helps us do that.
 '''
 AUTHOR = 'Toshio Kuratomi, Luke Macken, Ricky Elrod, Ralph Bean, Patrick Uiterwijk'
 EMAIL = 'admin@fedoraproject.org'
-COPYRIGHT = '2007-2017 Red Hat, Inc.'
+COPYRIGHT = '2007-2018 Red Hat, Inc.'
 URL = 'https://fedorahosted.org/python-fedora'
 DOWNLOAD_URL = 'https://pypi.python.org/pypi/python-fedora'
 LICENSE = 'LGPLv2+'
