3dae508d53e6fa8bcb08bcc0fc4aebf9dd512e92
Patrick Uiterwijk <patrick@puiterwijk.org>
2018-02-01 13:07:08 +0100

    Start nuking old framework support
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- fedora/tg/__init__.py
+++
@@ -1,6 +0,0 @@
-'''
-Functions and classes to help build a Fedora Service.
-'''
-
-__all__ = ('client', 'json', 'tg1utils', 'tg2utils', 'widgets',
-           'identity', 'utils', 'visit')
--- fedora/tg/visit/__init__.py
+++
@@ -1,3 +0,0 @@
-'''TurboGears visit manager to save visit in the Fedora Account System.'''
-
-__all__ = ('jsonfasvisit',)
